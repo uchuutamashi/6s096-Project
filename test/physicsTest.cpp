0d3aee81a60595b605a4bfa900fd42dd56fae8d8
To Chin Yu <ytc@mit.edu>
2014-01-29 18:54:45 -0500

    +tests for y, z directions

--- test/physicsTest.cpp
+++ test/physicsTest.cpp
@@ -6,7 +6,7 @@
 using namespace nbody;
 typedef std::vector<Body> tBodyVec;
 
-TEST( physicsTest, constAccel ) {
+TEST( physicsTest, constAccelx ) {
   tBodyVec testobj;
   testobj.resize(1);
   testobj[0].position() = Vector3d{0,0,0};
@@ -18,6 +18,38 @@ TEST( physicsTest, constAccel ) {
   for(int i=1; i<100; i++){
     testsys->integrateSystem( );
     EXPECT_LT(testsys->getPosition(0).x()-0.5*i*DELTA*i*DELTA, DELTA); //tolerate 1st order err for now 
+  };
+  delete testsys;
+}
+
+TEST( physicsTest, constAccely ) {
+  tBodyVec testobj;
+  testobj.resize(1);
+  testobj[0].position() = Vector3d{0,0,0};
+  testobj[0].velocity() = Vector3d{0,0,0};
+  testobj[0].accel()    = Vector3d{0,1,0};
+  
+  System *testsys = new System{testobj, Euler, DELTA};
+
+  for(int i=1; i<100; i++){
+    testsys->integrateSystem( );
+    EXPECT_LT(testsys->getPosition(0).y()-0.5*i*DELTA*i*DELTA, DELTA); //tolerate 1st order err for now 
+  };
+  delete testsys;
+}
+
+TEST( physicsTest, constAccelz ) {
+  tBodyVec testobj;
+  testobj.resize(1);
+  testobj[0].position() = Vector3d{0,0,0};
+  testobj[0].velocity() = Vector3d{0,0,0};
+  testobj[0].accel()    = Vector3d{0,0,1};
+  
+  System *testsys = new System{testobj, Euler, DELTA};
+
+  for(int i=1; i<100; i++){
+    testsys->integrateSystem( );
+    EXPECT_LT(testsys->getPosition(0).z()-0.5*i*DELTA*i*DELTA, DELTA); //tolerate 1st order err for now 
   };
   delete testsys;
 }
