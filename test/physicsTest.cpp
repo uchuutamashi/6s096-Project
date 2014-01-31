7533a139f104c6103a3a02298b2321bc87e88c03
gajjanag <gajjanagadde@gmail.com>
2014-01-31 09:01:24 -0500

    Changed the physics test, which was flawed. The reason is as follows. With only one body, there can be no gravity, and even if one sets the acceleration to a non-zero value, it gets overwritten (rightly) by 0 during the computation of gravity interactions. As such, the body stays put. Instead, I have added a test with a non-zero constant velocity, and zero acceleration (the value of acceleration is immaterial, in fact, I will add a test like that in the next commit to ensure this invariant). This corresponds to motion on a line, and this is tested.

--- test/physicsTest.cpp
+++ test/physicsTest.cpp
@@ -1,55 +1,26 @@
 #include <nbody/System.h>
 #include <gtest/gtest.h>
 
-#define DELTA 0.0001
-
+#define DELTA 1
+#define N 5
 using namespace nbody;
 typedef std::vector<Body> tBodyVec;
 
-TEST( physicsTest, constAccelx ) {
+TEST( physicsTest, constVelocity ) {
   tBodyVec testobj;
   testobj.resize(1);
   testobj[0].position() = Vector3d{0,0,0};
-  testobj[0].velocity() = Vector3d{0,0,0};
-  testobj[0].accel()    = Vector3d{1,0,0};
+  testobj[0].velocity() = Vector3d{1,2,3};
+  testobj[0].accel()    = Vector3d{0,0,0};
   
   System *testsys = new System{testobj, Euler, DELTA};
 
-  for(int i=1; i<100; i++){
-    testsys->integrateSystem( );
-    EXPECT_LT(testsys->getPosition(0).x()-0.5*i*DELTA*i*DELTA, DELTA); //tolerate 1st order err for now 
+  for( int i = 1; i < N; i++ ){
+    testsys->integrateSystem();
+    ASSERT_DOUBLE_EQ( testsys->getPosition(0).x()-i*DELTA, 0.0 );
+    ASSERT_DOUBLE_EQ( testsys->getPosition(0).y()-2*i*DELTA, 0.0 );
+    ASSERT_DOUBLE_EQ( testsys->getPosition(0).z()-3*i*DELTA, 0.0 );
   };
   delete testsys;
 }
 
-TEST( physicsTest, constAccely ) {
-  tBodyVec testobj;
-  testobj.resize(1);
-  testobj[0].position() = Vector3d{0,0,0};
-  testobj[0].velocity() = Vector3d{0,0,0};
-  testobj[0].accel()    = Vector3d{0,1,0};
-  
-  System *testsys = new System{testobj, Euler, DELTA};
-
-  for(int i=1; i<100; i++){
-    testsys->integrateSystem( );
-    EXPECT_LT(testsys->getPosition(0).y()-0.5*i*DELTA*i*DELTA, DELTA); //tolerate 1st order err for now 
-  };
-  delete testsys;
-}
-
-TEST( physicsTest, constAccelz ) {
-  tBodyVec testobj;
-  testobj.resize(1);
-  testobj[0].position() = Vector3d{0,0,0};
-  testobj[0].velocity() = Vector3d{0,0,0};
-  testobj[0].accel()    = Vector3d{0,0,1};
-  
-  System *testsys = new System{testobj, Euler, DELTA};
-
-  for(int i=1; i<100; i++){
-    testsys->integrateSystem( );
-    EXPECT_LT(testsys->getPosition(0).z()-0.5*i*DELTA*i*DELTA, DELTA); //tolerate 1st order err for now 
-  };
-  delete testsys;
-}
